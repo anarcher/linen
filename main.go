b9dde4225cc8ec9b358828120d0ee9c052cc4f78
anarcher <anarcher@gmail.com>
2015-03-15 18:37:25 +0900

    Add cli and logger

--- main.go
+++ main.go
@@ -1,35 +1,71 @@
 package main
 
 import (
-	"flag"
-	//	"github.com/codegangsta/cli"
-	"log"
+	"github.com/codegangsta/cli"
+	"github.com/mgutz/logxi/v1"
+	"os"
 )
 
-var srcPath = flag.String("src", ".", "source path (default: .)")
-var targetPath = flag.String("target", "./target", "target path (default: ./target)")
+var logger log.Logger
+
+var (
+	flSrcPath = cli.StringFlag{
+		Name:  "src,s",
+		Value: ".",
+		Usage: "",
+	}
+	flTargetPath = cli.StringFlag{
+		Name:  "target,t",
+		Value: "./target",
+		Usage: "",
+	}
+)
+
+func init() {
+	logger = log.New("linen")
+	logger.SetLevel(log.LevelInfo)
+}
 
 func main() {
 
-	flag.Parse()
+	app := cli.NewApp()
+	app.Name = "linen"
+	app.Usage = "is simple static page(s) generator"
+	app.Version = VERSION
+	app.Commands = []cli.Command{
+		{
+			Name:      "build",
+			ShortName: "b",
+			Usage:     "build source pages",
+			Flags:     []cli.Flag{flSrcPath, flTargetPath},
+			Action:    BuildAction,
+		},
+	}
+
+	app.Run(os.Args)
+}
+
+func BuildAction(c *cli.Context) {
+	srcPath := c.String("src")
+	targetPath := c.String("target")
 
 	var err error
-
 	var files Files
 
-	files, err = ReadFiles(*srcPath)
+	files, err = ReadFiles(srcPath)
 	if err != nil {
-		log.Println(err)
+		logger.Error("Read", "err", err)
 	}
 
 	err = TransformFiles(files)
 	if err != nil {
-		log.Println(err)
+		logger.Error("Transform", "err", err)
 	}
 
-	err = WriteFiles(files, *targetPath)
+	err = WriteFiles(files, targetPath)
 	if err != nil {
-		log.Println(err)
+		logger.Error("Write", "err", err)
 	}
 
+	logger.Info("DONE")
 }
